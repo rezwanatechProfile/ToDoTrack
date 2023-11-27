58a85ecca99704a66cff399a9125ee316d222e75
rezwanatech <rezwanatech@gmail.com>
2023-11-26 21:45:59 -0500

    add Priority feature and modify the CSS

---
+++ todo-app/src/actions/priorityActions.js
@@ -0,0 +1,12 @@
+// to handle prioritize a task and moving it to the priority list.
+
+// Dispatching this action would typically update the state to reflect that the task with the specified id is now priority.
+export const priorityTaskAction = (id) => ({
+  type: "PRIORITY_TASK",
+  payload: id,
+});
+
+export const moveToPriorityListAction = (id) => ({
+  type: "MOVE_TO_PRIORITY_LIST",
+  payload: id,
+});
