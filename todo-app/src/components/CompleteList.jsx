7f29a50bbc94f78f6a5b3bdb91a0836cbcd5738f
rezwanatech <rezwanatech@gmail.com>
2023-11-27 00:52:43 -0500

    add responsive style

--- todo-app/src/components/CompleteList.jsx
+++ todo-app/src/components/CompleteList.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Typography, List, ListItem, ListItemText, Paper, ListItemIcon } from "@mui/material";
+import { Typography, List, ListItem, ListItemText, Paper, ListItemIcon, Box} from "@mui/material";
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import Button from "@mui/material/Button";
 import { removeTaskAction } from "../actions/taskActions";
 
 
 export const CompleteList = () => {
-
   const completeList = useSelector((state) => state.task.completeList);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const removeTask = (id) => {
-		// your code here
-		dispatch(removeTaskAction(id));
-	};
-
+    // your code here
+    dispatch(removeTaskAction(id));
+  };
 
   return (
-    <Paper elevation={3} style={{ padding: "20px", margin: "20px", maxWidth: "400px", marginLeft: 'auto', marginRight: 'auto' }}>
+    <Paper elevation={3} style={{ padding: "20px", margin: "20px" }}>
       <Typography variant="h4" align="center" gutterBottom>
         Completed Tasks
       </Typography>
@@ -26,22 +24,21 @@ export const CompleteList = () => {
         {completeList.map((task) => (
           <ListItem key={task.id}>
             <ListItemIcon>
-                <CheckCircleOutlineIcon color="primary" />
-             </ListItemIcon>
+              <CheckCircleOutlineIcon color="primary" />
+            </ListItemIcon>
             <ListItemText primary={task.description} />
 
             <Button
-            variant="contained"
-            color="error"
-            onClick={() => dispatch(removeTaskAction(task.id))}
-          >
-             DELETE
-           </Button>
+              variant="contained"
+              color="error"
+              onClick={() => dispatch(removeTaskAction(task.id))}
+            >
+              DELETE
+            </Button>
           </ListItem>
-
-
         ))}
       </List>
     </Paper>
   );
 };
+
